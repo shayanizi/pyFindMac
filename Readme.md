6fae4a5176d25280c573329b36823706d949dfcd
shayanizii <izishayan@gmail.com>
2018-04-11 09:52:14 +0430

    be continue.....

---
+++ Readme.md
@@ -0,0 +1,12 @@
+this script can show all of mac addres in localhost 
+------------------------------------------------------
+FOR use it :
+
+1 >> install python2.7.*
+2 >> install moudel scapy { pip install scapy }
+3 >> then run the script and see results :))
+4 >> you can emmbeded it 
+
+GOOD LUCK 
+-----------------------------------------------------
+
---
+++ scanerRouter.py
@@ -0,0 +1,12 @@
+ #!/usr/bin/python2.7.14
+ #the first import scapy moduls
+from scapy.all import *
+from scapy.layers.l2 import Ether, ARP
+#create a loop for testing every ip in localhost
+for localHost in range(1,255):
+	ip="192.168.1."+str(localHost)
+	arpRequest=Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ip,hwdst="ff:ff:ff:ff:ff:ff")
+	arpResponse = srp1(arpRequest,timeout=1,verbose=0)
+	if arpResponse:
+		print "IP >> " + arpResponse.psrc + " MAC >> " + arpResponse.hwsrc
+#___________________________________________________________________________________________
